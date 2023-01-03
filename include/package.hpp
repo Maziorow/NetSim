4259260aa49b9ca14e18cab8989059f38c12de60
Mateusz Maź <410372@student.agh.edu.pl>
2023-01-03 22:12:47 +0100

    Pierwszy commit

---
+++ include/package.hpp
@@ -0,0 +1,11 @@
+#ifndef IMPLEMENTATION_PACKAGE_HPP
+#define IMPLEMENTATION_PACKAGE_HPP
+
+class Package{
+public:
+    Package();
+    Package(ElementID id);
+    Package(Package& package)
+};
+
+#endif //IMPLEMENTATION_PACKAGE_HPP
---
+++ include/types.hpp
@@ -0,0 +1,5 @@
+#ifndef IMPLEMENTATION_TYPES_HPP
+#define IMPLEMENTATION_TYPES_HPP
+
+using unsigned int = ElementID;
+#endif //IMPLEMENTATION_TYPES_HPP
